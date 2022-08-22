24711b978e5ef0557fb37991174c84f9484e1867
andikayaya2115 <111480065+andikayaya2115@users.noreply.github.com>
2022-08-21 22:01:56 -0700

    Add files via upload

---
+++ python4_andika satria.py
@@ -0,0 +1,16 @@
+n = input("masukan nama depan: ")
+b = input("masukan nama depan: ")
+print("nama lengkap anda adalah", n,b)
+
+a = int(input("masukan nilai a: "))
+b = int(input("masukan nilai b: "))
+c = a+b
+
+print("a =", a)
+print("b =", b)
+print(a,"+",b,"=" ,c)
+
+x = int(input("masukan mata uang: "))
+y = x * 13752
+print(x,"dollar=",y,"rupiah")
+
